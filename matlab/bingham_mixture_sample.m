3c6a653f92031a6f8c0d1bde17730e6d5cdd9850
madratman <ratneshmadaan@gmail.com>
2015-07-20 12:46:30 -0400

    fixes for opt arg of bing_mix_sample + minor comments

--- matlab/bingham_mixture_sample.m
+++ matlab/bingham_mixture_sample.m
@@ -1,24 +1,34 @@
 function [X X2 X3]= bingham_mixture_sample(BM,BM_weights,no_of_samples)
-% X = bingham_mixture_sample(B,W,no_of_samples) - sample no_of_samples points from a Bingham
+% samples no_of_samples points from a Bingham mixture with weights BM_weights
 % mixture using Monte Carlo simulation
 
+% X -> just the samples
+
+% Optional output arguments:
+% X2 -> index of the corresponding component in the BM from where the sample was sampled from
+% ^vector of length == no_of_samples
+% X3 -> no of samples from each component(vector of length == no_of_components)
+
 ind = zeros(no_of_samples, 1);
+no_of_components = length(BM);
+no_of_samples_from_component = zeros(no_of_components, 1);
 
 if no_of_samples==1
     % sample a mixture component
     i = pmf_sample(BM_weights);
     X = bingham_sample(BM(i), 1);
     ind(i) = i;
+    % no_of_samples_from_component = 1; %this line doesn't matter for no_of_samples>1. 
 
 elseif no_of_samples<100
     for i=1:no_of_samples
         j = pmf_sample(BM_weights);
         X(i,:) = bingham_mixture_sample(BM(j),1,1);
         ind(i) = j;
+        no_of_samples_from_component(j) = no_of_samples_from_component(j) + 1;
     end
 else  % no_of_samples >= 100
 
-    no_of_components = length(BM)
     for i=1:no_of_components
         no_of_samples_from_component(i) = round(no_of_samples*BM_weights(i));
     end
@@ -40,12 +50,13 @@ else  % no_of_samples >= 100
     end
     no_of_samples_from_component
     X = [];
-    for i=1:1
+    for i=1:no_of_components
         X = [X; bingham_sample(BM(i), no_of_samples_from_component(i))]
         i;
         no_of_samples_from_component(i);
     end
 
+    %to track origin of the sample
     for j = 1:no_of_components
         last = 0;
         for i=1:no_of_samples_from_component(j)
@@ -69,4 +80,4 @@ else  % no_of_samples >= 100
 end
 
 X2 = ind;
-% X3 = no_of_samples_from_component;
+X3 = no_of_samples_from_component;
