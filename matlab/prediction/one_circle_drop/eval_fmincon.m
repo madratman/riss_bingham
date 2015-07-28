d50ddb46d06a17ba5f6a2e0154e7d17af80f9a01
madratman <ratneshmadaan@gmail.com>
2015-07-28 19:21:49 -0400

    fmincon: fixes, comments

--- matlab/prediction/one_circle_drop/eval_fmincon.m
+++ matlab/prediction/one_circle_drop/eval_fmincon.m
@@ -1,2 +1,16 @@
-t_init = rand(6,1)
+
+global quat_input;
+
+no_of_quat_input = 3
+quat_input = quaternion.angleaxis(linspace(0,2*pi,no_of_quat_input), repmat([1 0 0],no_of_quat_input,1));
+
+t_init = rand(4+no_of_quat_input,1)
+
 req_var = fmincon(@obj,t_init,[],[],[],[],[],[],@nonlcon)
+
+quat_trans = [req_var(1) req_var(2) req_var(3) req_var(4)]
+
+for i = 5:length(req_var)
+    quat_final(i-4,:) = [cos(req_var(i)/2) 0 0 sin(req_var(i)/2)];
+end
+plot_quaternions_length(quat_final)
--- matlab/prediction/one_circle_drop/nonlcon.m
+++ matlab/prediction/one_circle_drop/nonlcon.m
@@ -4,11 +4,30 @@ c=[]; % coz no nonlinear inequalities
 
 theta = req_var(5:end);
 quat_trans =  req_var(1:4)';
-quat_input = quaternion.angleaxis(linspace(0,2*pi,360), repmat([1 0 0],360,1));
-quat_trans = quaternion(quat_trans);
+% req_var is the vector of the optimization variables.
+% The first four elements are just the transformation quaternion, which is
+% unknown. 
+% The elements from 5th onwards are the angles of the final quaternions in
+% the "plane normal to Z axis"
+
+global quat_input;
+
+quat_trans = quaternion(quat_trans); 
+
 for i = 1:length(theta)
     final_quat = [cos(theta(i)/2) 0 0 sin(theta(i)/2)];
+    % As the final_quat is in plane normal to Z axis.
     ceq(i,:) = from_quaternion_class_to_array(times(quat_input(i), quat_trans)) - final_quat;
+    % Rotate each element of quat_input by multiplying with quat_trans and
+    % subtract the final_quat from it     
 end
-quat_trans
-ceq
+
+% fmincon wants the ceq(the equality constraints) to be a vector. Right now
+% it is of size length(theta)*4. So we make it into a vector. 
+
+%%debug%%
+quat_trans;
+ceq;
+%%debug%%
+
+ceq = ceq(:)
